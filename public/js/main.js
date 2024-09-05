77a42f396d8c9a70e5b49088c6344a37a9b5962f
darkalbatrus <147101531+darkalbatrus@users.noreply.github.com>
2024-09-05 06:00:34 +0330

    convert to clean code

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,17 @@ const openButton = document.querySelector(".open-btn");
 const closeButton = document.querySelector(".close-btn");
 const modalBackdrop = document.querySelector(".modal-backdrop");
 
+//!-- Toggle Func --//
+const toggleModal = () => {
+  modalBackdrop.classList.toggle("show-modal");
+};
+
 //!-- Open Func --//
 openButton.addEventListener("click", () => {
-  modalBackdrop.classList.toggle("show-modal");
+  toggleModal();
 });
 
 //!-- Close Func --//
 closeButton.addEventListener("click", () => {
-  modalBackdrop.classList.toggle("show-modal");
+  toggleModal();
 });
