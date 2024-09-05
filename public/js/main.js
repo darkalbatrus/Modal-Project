3ee430bf6aa454d3bab4f734020decd21266ff3b
darkalbatrus <147101531+darkalbatrus@users.noreply.github.com>
2024-09-05 05:55:15 +0330

    add element to js

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+//?-- Element --//
+const openButton = document.querySelector(".open-btn");
+const closeButton = document.querySelector(".close-btn");
+const modalBackdrop = document.querySelector(".modal-backdrop");
