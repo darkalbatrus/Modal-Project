3baf39783bcd507d72d8b19bdcfec59d001c99c8
darkalbatrus <147101531+darkalbatrus@users.noreply.github.com>
2024-09-05 05:57:11 +0330

    add Open & Close Func to js

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,13 @@
 const openButton = document.querySelector(".open-btn");
 const closeButton = document.querySelector(".close-btn");
 const modalBackdrop = document.querySelector(".modal-backdrop");
+
+//!-- Open Func --//
+openButton.addEventListener("click", () => {
+  modalBackdrop.classList.add("show-modal");
+});
+
+//!-- Close Func --//
+closeButton.addEventListener("click", () => {
+  modalBackdrop.classList.remove("show-modal");
+});
